ec828802f32ba030b5f33325aa85bb442dcf0e00
Ahmad Ashfaq <spoiledwit@gmail.com>
2024-03-13 17:22:23 +0500

    added laravel api

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://ezifx-crm.vercel.app", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://ezifx-crm.vercel.app", "http://localhost:3000", "https://ezifx-crm-new.vercel.app"],
   }), 
 );
 
