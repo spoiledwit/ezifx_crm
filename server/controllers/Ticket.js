be7699d5d2498bb0b89106c5ccc5fd5f8029f9c6
173303-WebConcepts <hamzahameed519@gmail.com>
2024-03-23 14:02:16 +0500

    Ticket reply done

--- server/controllers/Ticket.js
+++ server/controllers/Ticket.js
@@ -66,11 +66,51 @@ export const getTicketDetails = async (req, res) => {
 
 export const getAllTickets = async (req, res) => {
   try {
-
-    const tickets = await Ticket.find()
+    const tickets = await Ticket.find();
 
     res.status(200).json(tickets);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  const { ticketId } = req.params;
+  const { content } = req.body;
+  const senderId = req.userId;
+
+  try {
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+      return res.status(404).json({ error: "Ticket not found" });
+    }
+
+    // Create a new message
+    const newMessage = {
+      content,
+      senderId,
+    };
+
+    ticket.messages.push(newMessage);
+
+    const existingUser =await AuthModel.findById(senderId);
+
+    console.log('333333', existingUser)
+
+    if (existingUser && existingUser.isAdmin) {
+      ticket.latestReply = content;
+
+    } else {
+      ticket.latestMessage = content;
+    }
+
+    // Save the updated ticket
+    await ticket.save();
+
+    res.status(201).json({ message: "Message sent successfully", ticket });
+  } catch (error) {
+    console.error("Error sending message:", error.message);
+    res.status(500).json({ error: error.message });
   }
 };
--- server/routes/ticket.js
+++ server/routes/ticket.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createAccount, getAccountDetails, getAllTransactions, getAllUserTransactions, getMyAccounts, getTransactions, getUserAccount, getUserAccounts } from "../controllers/Account.js";
-import { createTicket, getAllTickets, getTicketDetails, getUserTicket } from "../controllers/Ticket.js";
+import { createTicket, getAllTickets, getTicketDetails, getUserTicket, sendMessage } from "../controllers/Ticket.js";
 import verifyAdmin from "../middlewares/verifyAdmin.js";
 import verifyToken from "../middlewares/verifyToken.js";
 
@@ -10,6 +10,9 @@ router.post("/create", verifyToken, createTicket);
 router.get("/userTickets", verifyToken, getUserTicket);
 router.get("/:id", verifyToken, getTicketDetails);
 router.get("/",verifyToken, verifyAdmin,  getAllTickets);
+
+
+router.post("/send-message/:ticketId",verifyToken,  sendMessage);
 
 
 export default router;
